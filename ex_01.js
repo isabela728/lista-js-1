34885e3bf6b88a2804fee7624d5bb6d166506a48
Isabela <isabela_oliveira3@estudante.sesisenai.org.br>
2026-03-17 12:12:34 -0300

    add - adicionei exercício 1

---
+++ ex_01.js
@@ -0,0 +1,20 @@
+function somarNumeros(){
+    let soma = 0;
+    let continuar = true;
+
+    while (continuar){
+        let numero = parseFloat(prompt("Digite um número (ou digite 0 para finalizar)."));
+
+        if (numero==0){
+            alert("A soma dos números é: "+soma);
+        }
+        
+        if (!isNaN(numero)){
+            soma+=numero;
+        }else{
+            alert("Por favor, digite umnúmero válido!");
+        }
+        continuar=confirm("Deseja adicionar mais números?");
+    }
+    alert("A soma dos números é: "+soma);
+}
