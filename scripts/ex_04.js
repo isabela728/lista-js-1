aac9712a4d8722335ca2524867df9b3248694dad
Isabela <isabela_oliveira3@estudante.sesisenai.org.br>
2026-03-24 10:51:39 -0300

    update - refiz de forma mais rápida

--- scripts/ex_04.js
+++ scripts/ex_04.js
@@ -4,32 +4,13 @@ Ao final, o programa deve mostrar os números em ordem decrescente.*/
 function ordenar(){
 
     let numeros = [];
-    let resultado = "";
 
     for (let i = 0; i < 4; i++) {
-        numeros[i] = parseInt(prompt("Digite um número:"));
+        numeros.push(Number(prompt("Digite um número:")));
     }
 
-    for (let i = 0; i < 4; i++) {
-        for (let j = i + 1; j < 4; j++) {
-            
-            if (numeros[i] < numeros[j]) {
-                let temp = numeros[i];
-                numeros[i] = numeros[j];
-                numeros[j] = temp;
-            }
+    numeros.sort((a,b) =>b - a);
 
-        }
-    }
-
-    for (let i = 0; i < 4; i++) {
-        resultado += numeros[i];
-
-        if (i < 3) {
-            resultado += ", ";
-        }
-    }
-
-    alert(resultado);
+    alert("Ordem decrescente:"+numeros);
 
 }
